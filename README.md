d69fcc52f891ebea19b910ec3621d7c3cd8232fc
Adidas Hoodie <adidashoodiesecond@gmail.com>
2021-08-12 18:36:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# What is this?
+
+It's a simple Node.JS Binary to show in console Notifications with design.
+
+# Installation
+
+`npm i console-notifications --save`
+
+Then...
+
+```
+var notifications = require('console-notifications'); // Importing Package
+
+// Success
+notifications.Sucess("Success notification"); // arg 1 = Message
+notifications.Sucess("With Title", "Success notification"); // arg 1 = Message, arg 2 = Title
+
+// Warning
+notifications.Warning("Warning notification"); // arg 1 = Message
+notifications.Warning("With Title", "Warning notification"); // arg 1 = Message, arg 2 = Title
+
+// Error
+notifications.Error("Error notification"); // arg 1 = Message
+notifications.Error("With Title", "Error notification"); // arg 1 = Message, arg 2 = Title
+
+
+// Example command
+notifications.ExampleNotify();
+```
+
+## Options
+
+Console Notifications supports only 2 arguments, The 2nd argument is optional.
+
+* - ``Message`` The first argument is message which is message of the Notification.
+* ``Title`` The second argument is Title. Which is optional. It's title of Notificaiton.
+
+
+## In Progress
+
+Now i'm working on:
+@@ ``Making the arguments be automatically sized`` @@
---
+++ index.js
@@ -0,0 +1,437 @@
+//const ascitable = require('ascii-table');
+var string = "";
+var spaces = 36
+var decimalspaces = 0;
+
+function isdecimalint(integer) {
+    return !Number.isInteger(integer);
+}
+
+
+
+// Success Notification
+function Success(Message, Title) {
+    if(!Title){
+        if(Message.length <= 31) {
+            console.log('\x1b[32m', '')
+            console.log(".------------------------------------.")
+            console.log("|                 ✔                  |")
+            console.log(":------------------------------------:")
+            
+            string = "";
+            spaces = 36;
+
+            decimalspaces = 0;
+            decimalspaces = spaces - Message.length;
+            decimalspaces = decimalspaces / 2;
+
+            if (isdecimalint(decimalspaces)) {
+                decimalspaces = Number.parseInt(decimalspaces);
+                for (i = 0; i != decimalspaces; i++)
+                    string = string + " ";
+
+                console.log("|" + string + Message + string + " |");
+            }
+            else {
+                for (i = 0; i != decimalspaces; i++)
+                    string = string + " ";
+
+                console.log("|" + string + Message + string + "|");
+            }
+            spaces = 36;
+            string = "";
+            console.log("|                                    |")
+            console.log("'------------------------------------'")
+            console.log('\x1b[0m', '')
+        }
+        else {
+            console.log('\x1b[31m', '')
+            console.log(".------------------------------------.")
+            console.log("| 💥 |  An Error Has occured!        |")
+            console.log("| 💥 |  The argumetns length are     |")
+            console.log("| 💥 |  Too Much to make Notify run  |")
+            console.log("'------------------------------------'")
+            console.log('\x1b[0m', '')
+        }
+    }
+    else {
+        if (Message.length <= 31 && Title.length <= 31) {
+            console.log('\x1b[32m', '')
+            console.log(".------------------------------------.")
+            console.log("|                 ✔                  |")
+            console.log(":------------------------------------:")
+            spaces = 36;
+            string = "";
+            decimalspaces = 0;
+            decimalspaces = spaces - Title.length;
+            decimalspaces = decimalspaces / 2;
+            if (isdecimalint(decimalspaces))
+            {
+                decimalspaces = Number.parseInt(decimalspaces);
+                for (i = 0; i != decimalspaces; i++)
+                    string = string + " ";
+                console.log("|" + string + Title + string + " |");
+                console.log("|                                    |")
+
+                string = "";
+                decimalspaces = 0;
+                decimalspaces = spaces - Message.length;
+                decimalspaces = decimalspaces / 2;
+                if (isdecimalint(decimalspaces)) {
+                    decimalspaces = Number.parseInt(decimalspaces);
+                    for (i = 0; i != decimalspaces; i++)
+                        string = string + " ";
+                    console.log("|" + string + Message + string + " |");
+                }
+                else {
+                    for (i = 0; i != decimalspaces; i++)
+                        string = string + " ";
+                    console.log("|" + string + Message + string + "|");
+                }
+
+                console.log("'------------------------------------'")
+                decimalspaces = 0;
+                string = "";
+                console.log('\x1b[0m', '')
+            }
+            else {
+                for (i = 0; i != decimalspaces; i++)
+                    string = string + " ";
+                console.log("|" + string + Title + string + "|");
+                console.log("|                                    |")
+                string = "";
+                decimalspaces = 0;
+                decimalspaces = spaces - Message.length;
+                decimalspaces = decimalspaces / 2;
+                if (isdecimalint(decimalspaces)) {
+                    decimalspaces = Number.parseInt(decimalspaces);
+                    for (i = 0; i != decimalspaces; i++)
+                        string = string + " ";
+                    console.log("|" + string + Message + string + " |");
+                }
+                else {
+                    for (i = 0; i != decimalspaces; i++)
+                        string = string + " ";
+                    console.log("|" + string + Message + string + "|");
+                }
+                //console.log("|" + string + Message + string + "|");
+
+
+
+                console.log("'------------------------------------'")
+                decimalspaces = 0;
+                string = "";
+                console.log('\x1b[0m', '')
+            }
+            
+        }
+        else {
+            console.log('\x1b[31m', '')
+            console.log(".------------------------------------.")
+            console.log("| 💥 |  An Error Has occured!        |")
+            console.log("| 💥 |  The argumetns length are     |")
+            console.log("| 💥 |  Too Much to make Notify run  |")
+            console.log("'------------------------------------'")
+            console.log('\x1b[0m', '')
+        }
+    }
+}
+
+
+// Warning Notification
+function Warning(Message, Title) {
+    if(!Title){
+        if(Message.length <= 31) {
+            console.log('\x1b[33m', '')
+            console.log(".------------------------------------.")
+            console.log("|                 ⚠                  |")
+            console.log(":------------------------------------:")
+            
+            string = "";
+            spaces = 36;
+
+            decimalspaces = 0;
+            decimalspaces = spaces - Message.length;
+            decimalspaces = decimalspaces / 2;
+
+            if (isdecimalint(decimalspaces)) {
+                decimalspaces = Number.parseInt(decimalspaces);
+                for (i = 0; i != decimalspaces; i++)
+                    string = string + " ";
+
+                console.log("|" + string + Message + string + " |");
+            }
+            else {
+                for (i = 0; i != decimalspaces; i++)
+                    string = string + " ";
+
+                console.log("|" + string + Message + string + "|");
+            }
+            spaces = 36;
+            console.log("|                                    |")
+            console.log("'------------------------------------'")
+            console.log('\x1b[0m', '')
+        }
+        else {
+            console.log('\x1b[31m', '')
+            console.log(".------------------------------------.")
+            console.log("| 💥 |  An Error Has occured!        |")
+            console.log("| 💥 |  The argumetns length are     |")
+            console.log("| 💥 |  Too Much to make Notify run  |")
+            console.log("'------------------------------------'")
+            console.log('\x1b[0m', '')
+        }
+    }
+    else {
+        if (Message.length <= 31 && Title.length <= 31) {
+            spaces = spaces - 2;
+            spaces = spaces - Title.length;
+            for (i = 0; i != spaces; i++)
+                string = string + " ";
+            spaces = 32;
+            console.log('\x1b[33m', '')
+            console.log(".------------------------------------.")
+            console.log("|                 ⚠                  |")
+            console.log(":------------------------------------:")
+            string = "";
+            spaces = spaces - 2;
+            spaces = spaces - Message.length;
+            for (i = 0; i != spaces; i++)
+                string = string + " ";
+            spaces = 36;
+            string = "";
+            decimalspaces = 0;
+            decimalspaces = spaces - Title.length;
+            decimalspaces = decimalspaces / 2;
+            if (isdecimalint(decimalspaces))
+            {
+                decimalspaces = Number.parseInt(decimalspaces);
+                for (i = 0; i != decimalspaces; i++)
+                    string = string + " ";
+                console.log("|" + string + Title + string + " |");
+                console.log("|                                    |")
+
+                string = "";
+                decimalspaces = 0;
+                decimalspaces = spaces - Message.length;
+                decimalspaces = decimalspaces / 2;
+                if (isdecimalint(decimalspaces)) {
+                    decimalspaces = Number.parseInt(decimalspaces);
+                    for (i = 0; i != decimalspaces; i++)
+                        string = string + " ";
+                    console.log("|" + string + Message + string + " |");
+                }
+                else {
+                    for (i = 0; i != decimalspaces; i++)
+                        string = string + " ";
+                    console.log("|" + string + Message + string + "|");
+                }
+
+                console.log("'------------------------------------'")
+                decimalspaces = 0;
+                string = "";
+                console.log('\x1b[0m', '')
+            }
+            else {
+                for (i = 0; i != decimalspaces; i++)
+                    string = string + " ";
+                console.log("|" + string + Title + string + "|");
+                console.log("|                                    |")
+                string = "";
+                decimalspaces = 0;
+                decimalspaces = spaces - Message.length;
+                decimalspaces = decimalspaces / 2;
+                if (isdecimalint(decimalspaces)) {
+                    decimalspaces = Number.parseInt(decimalspaces);
+                    for (i = 0; i != decimalspaces; i++)
+                        string = string + " ";
+                    console.log("|" + string + Message + string + " |");
+                }
+                else {
+                    for (i = 0; i != decimalspaces; i++)
+                        string = string + " ";
+                    console.log("|" + string + Message + string + "|");
+                }
+                //console.log("|" + string + Message + string + "|");
+
+
+
+                console.log("'------------------------------------'")
+                decimalspaces = 0;
+                string = "";
+                console.log('\x1b[0m', '')
+            }
+            
+        }
+        else {
+            console.log('\x1b[31m', '')
+            console.log(".------------------------------------.")
+            console.log("| 💥 |  An Error Has occured!        |")
+            console.log("| 💥 |  The argumetns length are     |")
+            console.log("| 💥 |  Too Much to make Notify run  |")
+            console.log("'------------------------------------'")
+            console.log('\x1b[0m', '')
+        }
+    }
+}
+
+
+// Error Notification
+function Error(Message, Title) {
+    if (!Title) {
+        if (Message.length <= 31) {
+            console.log('\x1b[31m', '')
+            console.log(".------------------------------------.")
+            console.log("|                 ❌                 |")
+            console.log(":------------------------------------:")
+
+            string = "";
+            spaces = 36;
+
+            decimalspaces = 0;
+            decimalspaces = spaces - Message.length;
+            decimalspaces = decimalspaces / 2;
+
+            if (isdecimalint(decimalspaces)) {
+                decimalspaces = Number.parseInt(decimalspaces);
+                for (i = 0; i != decimalspaces; i++)
+                    string = string + " ";
+
+                console.log("|" + string + Message + string + " |");
+            }
+            else {
+                for (i = 0; i != decimalspaces; i++)
+                    string = string + " ";
+
+                console.log("|" + string + Message + string + "|");
+            }
+            spaces = 36;
+            console.log("|                                    |")
+            console.log("'------------------------------------'")
+            console.log('\x1b[0m', '')
+        }
+        else {
+            console.log('\x1b[31m', '')
+            console.log(".------------------------------------.")
+            console.log("| 💥 |  An Error Has occured!        |")
+            console.log("| 💥 |  The argumetns length are     |")
+            console.log("| 💥 |  Too Much to make Notify run  |")
+            console.log("'------------------------------------'")
+            console.log('\x1b[0m', '')
+        }
+    }
+    else {
+        if (Message.length <= 31 && Title.length <= 31) {
+            spaces = spaces - 2;
+            spaces = spaces - Title.length;
+            for (i = 0; i != spaces; i++)
+                string = string + " ";
+            spaces = 32;
+            console.log('\x1b[31m', '')
+            console.log(".------------------------------------.")
+            console.log("|                 ❌                 |")
+            console.log(":------------------------------------:")
+            string = "";
+            spaces = spaces - 2;
+            spaces = spaces - Message.length;
+            for (i = 0; i != spaces; i++)
+                string = string + " ";
+            spaces = 36;
+            string = "";
+            decimalspaces = 0;
+            decimalspaces = spaces - Title.length;
+            decimalspaces = decimalspaces / 2;
+            if (isdecimalint(decimalspaces)) {
+                decimalspaces = Number.parseInt(decimalspaces);
+                for (i = 0; i != decimalspaces; i++)
+                    string = string + " ";
+                console.log("|" + string + Title + string + " |");
+                console.log("|                                    |")
+
+                string = "";
+                decimalspaces = 0;
+                decimalspaces = spaces - Message.length;
+                decimalspaces = decimalspaces / 2;
+                if (isdecimalint(decimalspaces)) {
+                    decimalspaces = Number.parseInt(decimalspaces);
+                    for (i = 0; i != decimalspaces; i++)
+                        string = string + " ";
+                    console.log("|" + string + Message + string + " |");
+                }
+                else {
+                    for (i = 0; i != decimalspaces; i++)
+                        string = string + " ";
+                    console.log("|" + string + Message + string + "|");
+                }
+
+                console.log("'------------------------------------'")
+                decimalspaces = 0;
+                string = "";
+                console.log('\x1b[0m', '')
+            }
+            else {
+                for (i = 0; i != decimalspaces; i++)
+                    string = string + " ";
+                console.log("|" + string + Title + string + "|");
+                console.log("|                                    |")
+                string = "";
+                decimalspaces = 0;
+                decimalspaces = spaces - Message.length;
+                decimalspaces = decimalspaces / 2;
+                if (isdecimalint(decimalspaces)) {
+                    decimalspaces = Number.parseInt(decimalspaces);
+                    for (i = 0; i != decimalspaces; i++)
+                        string = string + " ";
+                    console.log("|" + string + Message + string + " |");
+                }
+                else {
+                    for (i = 0; i != decimalspaces; i++)
+                        string = string + " ";
+                    console.log("|" + string + Message + string + "|");
+                }
+                //console.log("|" + string + Message + string + "|");
+
+
+
+                console.log("'------------------------------------'")
+                decimalspaces = 0;
+                string = "";
+                console.log('\x1b[0m', '')
+            }
+
+        }
+        else {
+            console.log('\x1b[31m', '')
+            console.log(".------------------------------------.")
+            console.log("| 💥 |  An Error Has occured!        |")
+            console.log("| 💥 |  The argumetns length are     |")
+            console.log("| 💥 |  Too Much to make Notify run  |")
+            console.log("'------------------------------------'")
+            console.log('\x1b[0m', '')
+        }
+    }
+}
+
+
+
+//Notification test
+//success("Test 1 Run Successflye");
+
+
+//Notify Example Command
+function NotificationExample() {
+    Success("Example Notification");
+    Success("With Title.", "Example Notification")
+    Warning("Example Notification");
+    Warning("With Title.", "Example Notification")
+    Error("Example Notification");
+    Error("With Title.", "Example Notification")
+}
+NotificationExample();
+
+
+// Exports
+module.exports.Sucesss = Success;
+module.exports.Warning = Warning;
+module.exports.Error = Error;
+module.exports.ExampleNotify = NotificationExample;
